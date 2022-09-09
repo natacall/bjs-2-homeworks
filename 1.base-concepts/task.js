ca40d16e9296d791c727e03b429784713c57d154
Natali <shatabalova2011@gmail.com>
2022-09-09 23:58:41 +0700

    first commit

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,8 +1,20 @@
+"use strict";
+
 function solveEquation(a, b, c) {
-  let arr;
-  // код для задачи №1 писать здесь
+  let arr = [];
+  let result = Math.pow(b, 2) - 4 * a * c;
+
+  if (result < 0) {
+  } else if (result == 0) {
+    arr = [-b / (2 * a)]
+  } else if (result > 0) {
+    arr.push((-b + Math.sqrt(result)) / (2 * a));
+    arr.push((-b - Math.sqrt(result)) / (2 * a));
+  }
+  
   return arr; // array
 }
+solveEquation(4, 18, 9)
 
 function calculateTotalMortgage(percent, contribution, amount, date) {
   let totalAmount;
