8329d02f1a70cab8c362e50a595eb26535eafee4
Natali <shatabalova2011@gmail.com>
2022-09-17 08:37:26 +0700

    working with arrays

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,15 +1,15 @@
+'use strict'
+
 function compareArrays(arr1, arr2) {
-  let result;
-
-  // Ваш код
-
-  return result; // boolean
-}
+  let result = true;
+  result = arr1.every((elem, index) => elem === arr2[index]) && arr1.length === arr2.length;
+  
+ return result; 
+};
 
 function advancedFilter(arr) {
-  let resultArr;
-
-  // Ваш код
-
-  return resultArr; // array
-}
+  let resultArr = [];
+  resultArr = arr.filter(elem => elem > 0).filter(elem => elem % 3 === 0).map(elem => elem * 10)
+  
+  return resultArr; 
+};
