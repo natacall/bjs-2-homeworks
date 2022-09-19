d53480c945f2ddc9f747ee6273ca072d4649a4ab
Natali <shatabalova2011@gmail.com>
2022-09-19 19:50:57 +0700

    homework on objects

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,15 +1,13 @@
 'use strict'
 
 function compareArrays(arr1, arr2) {
-  let result = true;
-  result = arr1.every((elem, index) => elem === arr2[index]) && arr1.length === arr2.length;
   
- return result; 
+  return arr1.every((elem, index) => elem === arr2[index]) && arr1.length === arr2.length;
+  
 };
 
 function advancedFilter(arr) {
-  let resultArr = [];
-  resultArr = arr.filter(elem => elem > 0).filter(elem => elem % 3 === 0).map(elem => elem * 10)
   
-  return resultArr; 
+  return arr.filter(elem => elem > 0).filter(elem => elem % 3 === 0).map(elem => elem * 10)
+   
 };
--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,10 +1,39 @@
-function Student(name, gender, age) {
-    // Ваш код
+'use strict'
 
+function Student(name, gender, age) {
+    this.name = name;
+    this.gender = gender;
+    this.age = age;
 }
+let student3 = new Student("Oleg","male",29)
 
 Student.prototype.setSubject = function (subjectName) {
-  //ваш код
+  this.subject = subjectName;
 }
 
-// ваш код для остальных методов
+Student.prototype.addMark = function(mark) {
+  if ( this.marks === undefined) {
+  this.marks = [mark]; 
+    } else {
+  this.marks.push(mark); 
+  }
+}
+
+Student.prototype.addMarks = function(...mark){
+   this.marks = mark;
+}
+
+Student.prototype.getAverage = function(){
+  let avg = 0;
+  for (let elem in this.marks) {
+    avg += this.marks[elem];
+  }
+  return avg / this.marks.length;
+}  
+
+Student.prototype.exclude = function(reason){
+    delete Student.subject;
+    delete Student.marks;
+    this.excluded = reason;
+} 
+
