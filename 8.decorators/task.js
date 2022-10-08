d094a8f30f77bbae0d3212dcf6798b5addab27f8
Natali <shatabalova2011@gmail.com>
2022-10-08 09:39:26 +0700

    house work decorators

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -1,8 +1,47 @@
-function cachingDecoratorNew(func) {
-  // Ваш код
-}
+'use strict'
+
+  function cachingDecoratorNew(func) {
+    let cache = [];
+    function wrapper(...args) {
+      let hash = args.join(','); 
+      let objectInCache = cache.find(cacheItem => cacheItem.hash === hash); 
+      if (objectInCache) {
+        console.log('Из кэша: ' + objectInCache.result);
+        return 'Из кэша: ' + objectInCache.result;
+      }
+      let result = func.call(this, ...args);
+      console.log('Вычисляем: ' + result);
+      cache.push({ hash, result }); 
+      if (cache.length > 5) {
+        cache.shift() 
+      }
+      return 'Вычисляем: ' + result;
+    }
+    return wrapper;
+  }
+
+  function debounceDecoratorNew(func) {
+    let flag = false;
+    
+    return function wrapper(...args) {
+      wrapper.count++;
+      if (flag) {
+        return;
+      }
+      func.call(this, ...args);
+      flag = true;
+      setTimeout(() => {
+        flag = false;
+        wrapper.count++;
+        func.call(this, ...args)
+      });
+      wrapper.count = 0;
+      return wrapper;
+    }
+  }
 
 
-function debounceDecoratorNew(func) {
-  // Ваш код
-}
+
+
+
+
